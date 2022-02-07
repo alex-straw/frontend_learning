d1fb1e5b6cb65c1fecea801b1e68587a46c82292
Alex Straw <as17163@bristol.ac.uk>
2022-02-07 20:00:49 +0000

    making more progress - adding validation stages for input

--- app.js
+++ app.js
@@ -3,19 +3,45 @@ const userBet = document.querySelector('.btn');
 userBet.addEventListener('click', betPlaced); // On button click
 
 function betPlaced() {
-    var betSize = document.getElementById("betAmt").value;
-    alertUser(flipCoin())
+    var  _betSize = document.getElementById("betAmt").value;
+    var _walletAddress = document.getElementById("walletAddress").value;
+
+    alertUser(_walletAddress);
+    
+    if (validWalletAddress(_walletAddress)) {
+        alertUser("valid ")
+    }
+
+    alert(flipCoin(), _betSize);
+
+}
+
+function validBet (_betSize) {
+    if (typeof _betSize === 'number') {
+        return true;
+    }
+    return false;
+}
+
+function validWalletAddress (_walletAddress) {
+    var exampleAddress = "0xF1C37BC188643DF4Bf15Fd437096Eb654d30abc1"
+    if (_walletAddress.substring(0, 2) == "0x") {
+        if (_walletAddress.length() == exampleAddress.length()) {
+            return true;
+        }
+    }
+    return false;
 }
 
 function flipCoin() {
     return flipResult = Math.floor(Math.random() * 2);
 }
 
-function alertUser(flipResult) {
+function alertUser(flipResult, _betSize) {
     if (flipResult == 0) {
-        alert("You Lose");
+        alert("You Lose: " + (_betSize).toString() + " Ether");
     }
     if (flipResult == 1) {
-        alert("You Win")
+        alert("You Win: " + (_betSize*2).toString() + " Ether");
     }
 }
